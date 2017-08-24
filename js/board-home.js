5eec5e6ecea495ffc7c642410c5b3362d9d6dbf9
Jiying <jiying.qi@yahoo.com>
2017-08-24 19:28:17 -0400

    alyssa

---
+++ js/board-home.js
@@ -0,0 +1,27 @@
+window.onload = function;
+
+function() {
+
+  circleCont.href = "https://example.com";
+  circleCont.class = "svg";
+  document.getElementsByTagName('svg')[0].appendChild(circleCont);
+
+  var circlePattern = document.createElement('circle');
+  circlePattern.cx = "25%";
+  circlePattern.cy="340px";
+  circlePattern.r="8%";
+  circlePattern.fill="transparent";
+  circlePattern.stroke="lightblue";
+  circlePattern.stroke-width="0.5%";
+  var circleText = document.createElement('text');
+  circleText.x ="25%";
+  circleText.y="340px";
+  circleText.style.text-anchor="middle";
+  circleText.style.fill="lightskyblue";
+  circleText.innerHTML="hello!";
+
+  // The variable iDiv is still good... Just append to it.
+  circleA.appendChild(circlePattern);
+  circleA.appendChild(circleText)
+
+}
