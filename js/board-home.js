c900fe4b82dc41bbdcbfc0cb59ca5baae9bea704
Jiying <jiying.qi@yahoo.com>
2017-08-25 20:31:58 -0400

    alyssa

--- js/board-home.js
+++ js/board-home.js
@@ -2,8 +2,6 @@ var count = $("#graph a").length;
 for(i=0; i<count; i++){
     var xPos = Math.random() * (92 - 8) + 8;
     var yPos = Math.random() * (92 - 8) + 8;
-    alert(xPos);
-    alert(yPos);
     document.getElementsByTagName("text")[i].setAttribute("x", xPos+"%");
     document.getElementsByTagName("text")[i].setAttribute("y", yPos+"%");
     document.getElementsByTagName("circle")[i].setAttribute("cx", xPos+"%");
