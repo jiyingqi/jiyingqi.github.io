4f0f7b2f3cabc93016f15b029a58526e4390e577
Jiying <jiying.qi@yahoo.com>
2017-08-25 20:29:01 -0400

    alyssa

--- js/board-home.js
+++ js/board-home.js
@@ -1,13 +1,11 @@
-function getRandomPosition() {
-	var count = $("#graph a").length;
-	for(i=0; i<count; i++){
-      var xPos = Math.random() * (92 - 8) + 8;
-      var yPos = Math.random() * (92 - 8) + 8;
-      alert(xPos);
-      alert(yPos);
-      document.getElementsByTagName("text")[i].setAttribute("x", xPos+"%");
-      document.getElementsByTagName("text")[i].setAttribute("y", yPos+"%");
-      document.getElementsByTagName("circle")[i].setAttribute("cx", xPos+"%");
-      document.getElementsByTagName("circle")[i].setAttribute("cy", yPos+"%");
-    }
-}
+var count = $("#graph a").length;
+for(i=0; i<count; i++){
+    var xPos = Math.random() * (92 - 8) + 8;
+    var yPos = Math.random() * (92 - 8) + 8;
+    alert(xPos);
+    alert(yPos);
+    document.getElementsByTagName("text")[i].setAttribute("x", xPos+"%");
+    document.getElementsByTagName("text")[i].setAttribute("y", yPos+"%");
+    document.getElementsByTagName("circle")[i].setAttribute("cx", xPos+"%");
+    document.getElementsByTagName("circle")[i].setAttribute("cy", yPos+"%");
+  }
