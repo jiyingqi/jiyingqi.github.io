1919d433cf6e2fdb4b3d1fc160f147ded2e43043
Jiying <jiying.qi@yahoo.com>
2017-08-28 02:48:49 -0400

    picture

--- js/alyssapic-home.js
+++ js/alyssapic-home.js
@@ -1,12 +1,8 @@
 var count = 22;
 for(i=1; i<=count; i++){
-    var xPos = Math.random() * (92 - 8) + 8;
-    var yPos = Math.random() * (92 - 16) + 16;
     var elem = document.createElement("img");
     elem.setAttribute("src", "/alyssa/pic/picture/" + i + ".jpg");
-    elem.setAttribute("x", xPos+"%");
-    elem.setAttribute("y", yPos+"%");
-    elem.style.width = "10%";
+    elem.style.width = "20%";
     elem.style.height = "auto";
     //elem.setAttribute("max-width", "10%");
     //elem.setAttribute("height", "auto");
