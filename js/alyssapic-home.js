bc46767ab14236ad9c4dac2147d5c05120d2be93
Jiying <jiying.qi@yahoo.com>
2017-08-28 02:28:04 -0400

    picture

--- js/alyssapic-home.js
+++ js/alyssapic-home.js
@@ -3,7 +3,7 @@ for(i=1; i<=count; i++){
     var xPos = Math.random() * (92 - 8) + 8;
     var yPos = Math.random() * (92 - 16) + 16;
     var elem = document.createElement("img");
-    elem.setAttribute("src", "http://img.zohostatic.com/discussions/v1/images/defaultPhoto.png");
+    elem.setAttribute("src", i + ".jpg");
     elem.setAttribute("x", xPos+"%");
     elem.setAttribute("y", yPos+"%");
     elem.setAttribute("height", "auto");
